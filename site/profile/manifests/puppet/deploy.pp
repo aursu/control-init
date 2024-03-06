3c2be9669d6ffbbba9f8eec8272c39ca6e30e01b
Oleksandr Ursu <alexander.ursu@gmail.com>
2024-03-06 16:37:34 +0100

    Optimized profile::puppet::deploy code

--- site/profile/manifests/puppet/deploy.pp
+++ site/profile/manifests/puppet/deploy.pp
@@ -1,53 +1,8 @@
 class profile::puppet::deploy {
-  $access_data = lookup({
-      name          => 'profile::puppet::deploy::access',
-      value_type    => Array[
-        Struct[{
-            name                  => Stdlib::Fqdn,
-            key_data              => String,
-            key_prefix            => String,
-            Optional[server]      => Stdlib::Fqdn,
-            Optional[sshkey_type] => Openssh::KeyID,
-            Optional[ssh_options] => Openssh::SshConfig,
-        }]
-      ],
-      default_value => [],
-  })
-
-  $ssh_config = lookup({
-      name          => 'profile::puppet::deploy::ssh_config',
-      value_type    => Array[Openssh::SshConfig],
-      default_value => [],
-  })
-
-  $access_data.each |$creds| {
-    $key_name    = $creds['name']
-    $sshkey_type = $creds['sshkey_type'] ? { String => $creds['sshkey_type'], default => 'ed25519' }
-
-    openssh::priv_key { $key_name:
-      user_name   => 'root',
-      key_prefix  => $creds['key_prefix'],
-      sshkey_type => $sshkey_type,
-      key_data    => $creds['key_data'],
-    }
+  class { 'puppet::server::bootstrap::globals':
+    access_data => lookup({ name => 'profile::puppet::deploy::access', default_value => [] }),
+    ssh_config  => lookup({ name => 'profile::puppet::deploy::ssh_config', default_value => [] }),
   }
 
-  $ssh_access_config = $access_data.reduce([]) |$memo, $creds| {
-    $key_name    = $creds['name']
-    $server      = $creds['server'] ? { String => $creds['server'], default => downcase($key_name) }
-    $key_prefix  = $creds['key_prefix']
-    $sshkey_type = $creds['sshkey_type'] ? { String => $creds['sshkey_type'], default => 'ed25519' }
-
-    $config      = {
-      'Host'                  => $server,
-      'StrictHostKeyChecking' => 'no',
-      'IdentityFile'          => "~/.ssh/${key_prefix}.id_${sshkey_type}",
-    } + $creds['ssh_options']
-
-    $memo + [$config]
-  }
-
-  openssh::ssh_config { 'root':
-    ssh_config => $ssh_config + $ssh_access_config,
-  }
+  contain puppet::server::bootstrap::ssh
 }
