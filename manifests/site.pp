a890aec03cfc4b51e7ee05f47eacb3cdde4362e4
Oleksandr Ursu <alexander.ursu@gmail.com>
2024-04-03 22:36:29 +0200

    Added concept of server type (could be set via ENC)

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,18 @@
-# ENC script for unknown hosts returns "production" environment, therefore
-# puppet agent will install Puppet server on unknown host. To prevent this, add
-# condition
+# If the ENC (External Node Classifier) identifies a host, it sets the 'stype'
+# top-scope variable to dictate the server's role. In cases where 'stype' is
+# not defined (indicating an unrecognized host), the script defaults to
+# checking whether the host's IP matches the Puppet server's IP ('$::serverip').
+# If there's a match, the 'role::puppet::master' class is applied. This
+# mechanism ensures that only recognized hosts or the Puppet server itself are
+# configured accordingly. This strategy helps prevent the accidental
+# configuration of critical roles on unintended hosts. For guidance on Puppet's
+# built-in variables and facts, see:
 # https://www.puppet.com/docs/puppet/8/lang_facts_builtin_variables#lang_facts_builtin_variables-server-facts
-if $::serverip == $facts['networking']['ip'] {
+# lint:ignore:top_scope_facts
+if defined('$::stype') {
+  include "role::${::stype}"
+}
+elsif $::serverip == $facts['networking']['ip'] {
   include role::puppet::master
 }
+# lint:endignore
