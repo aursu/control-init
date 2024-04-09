3ee7c3254323443b965ce579aba78936a81e475f
Oleksandr Ursu <alexander.ursu@gmail.com>
2024-04-09 03:14:33 +0200

    Added ability to use external PuppetDB during Puppet master bootstrap

--- site/profile/manifests/puppet/master.pp
+++ site/profile/manifests/puppet/master.pp
@@ -5,22 +5,25 @@
 #
 class profile::puppet::master (
   String $platform_name = 'puppet8',
+  Boolean $puppetdb_local = true,
+  Stdlib::Host $puppetdb_server = 'puppet',
 ) {
   class { 'puppet::profile::puppet':
-    platform_name  => $platform_name,
+    platform_name   => $platform_name,
 
-    puppetserver   => true,
-    server         => 'puppet',
+    puppetserver    => true,
+    server          => 'puppet',
 
-    hosts_update   => true,
+    hosts_update    => true,
 
-    sameca         => true,
-    ca_server      => 'puppet',
+    sameca          => true,
+    ca_server       => 'puppet',
 
-    use_common_env => true,
+    use_common_env  => true,
 
-    puppetdb_local => true,
-    use_puppetdb   => true,
+    use_puppetdb    => true,
+    puppetdb_local  => $puppetdb_local,
+    puppetdb_server => $puppetdb_server,
   }
 
   include profile::puppet::deploy
